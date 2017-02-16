719a5566e1d6d5954e42046449bd0b1bc2456f3d
vmunnangi <venkata.munnangi@ascentis.com>
2017-02-15 18:04:08 -0800

    Changed the dependency of minHeap solution to Java's PriorityQueue.

--- src/Int/MaxNoOrderSum.java
+++ src/Int/MaxNoOrderSum.java
@@ -1,5 +1,7 @@
 package Int;
 
+import java.util.PriorityQueue;
+
 /**
  * Deepthi's Question
  * Find k max numbers within an array of size n.
@@ -10,7 +12,7 @@ package Int;
 public class MaxNoOrderSum {
 
   // Running time should be proportional to n log k, where n is the number of numbers and k is the maximum number of items on the heap
-    public void solution(int [] numbers, int k, MinHeap myHeap) {
+    public void solution(int [] numbers, int k, PriorityQueue<Integer> myHeap) {
         if(k > numbers.length) {
             // Error u cant have k numbers in (k>n) array.
             return;
@@ -21,7 +23,7 @@ public class MaxNoOrderSum {
         {
             if (totalFound < k) {
                 //add this number no matter what. Since, we need to totalFound is less than the target(k)
-                myHeap.insert(num);
+                myHeap.add(num);
                 totalFound++;
             } else if (num > myHeap.peek()) {
                 // If this number is larger than the smallest
@@ -29,7 +31,7 @@ public class MaxNoOrderSum {
                 // then remove the smallest number and add this one
                 myHeap.remove();
                 totalFound--;
-                myHeap.insert(num);
+                myHeap.add(num);
                 totalFound++;
             }
         }
@@ -37,7 +39,7 @@ public class MaxNoOrderSum {
     // At this point, the max numbers are on the heap,
     // but there might be too many of them.
     // Remove the smallest number until we have k numbers.
-        while ((totalFound - myHeap.getSize()) > k) {
+        while ((totalFound - myHeap.size()) > k) {
             myHeap.remove();
             totalFound--;
         }
@@ -51,23 +53,36 @@ public class MaxNoOrderSum {
         int [] array = {1,4,11,2,9};
         int k = 3;
 
-        MinHeap myHeap = new MinHeap(array.length);
+        PriorityQueue<Integer> myHeap = new PriorityQueue<Integer>(array.length);
         maxNoOrderSum.solution(array, k, myHeap);
-        myHeap.print();
+        maxNoOrderSum.print(myHeap);
 
         System.out.println("---------");
 
         int [] negativeArray = {1,-10, -5, 4, 2,-11};
-        myHeap = new MinHeap(negativeArray.length);
+        myHeap = new PriorityQueue<Integer>(negativeArray.length);
         maxNoOrderSum.solution(negativeArray, k, myHeap);
-        myHeap.print();
+        maxNoOrderSum.print(myHeap);
+
+
+        System.out.println("-------");
+        int [] numbers = {5,3,17,10,84,19,6,22,9};
+        myHeap = new PriorityQueue<Integer>(numbers.length);
+        maxNoOrderSum.solution(numbers, k, myHeap);
+        maxNoOrderSum.print(myHeap);
 
         System.out.println("-------Edge cases------");
 
         int [] twoNumbers = {1,-10};
         k = 4;
-        myHeap = new MinHeap(twoNumbers.length);
+        myHeap = new PriorityQueue<Integer>(twoNumbers.length);
         maxNoOrderSum.solution(twoNumbers, k, myHeap);
-        myHeap.print();
+        maxNoOrderSum.print(myHeap);
+    }
+
+    void print(PriorityQueue<Integer> pq) {
+        for(int i : pq) {
+            System.out.println(i);
+        }
     }
 }
