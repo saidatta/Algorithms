aa7d64da7020c698bdef73db6f54b0958c10ca7b
vmunnangi <venkata.munnangi@ascentis.com>
2017-04-02 17:24:54 -0700

    ADding solutions.

--- src/Leetcode/SlidingWindowMaximum.java
+++ src/Leetcode/SlidingWindowMaximum.java
@@ -10,22 +10,7 @@ import java.util.Deque;
  */
 public class SlidingWindowMaximum {
     /**
-     * We scan the array from 0 to n-1, keep "promising" elements in the deque.
-     * The algorithm is amortized O(n) as each element is put and polled once.
-     * At each i, we keep "promising" elements, which are potentially max number
-     * in window [i-(k-1),i] or any subsequent window. This means
      *
-     * If an element in the deque and it is out of i-(k-1), we discard them.
-     * We just need to poll from the head, as we are using a deque and elements are
-     * ordered as the sequence in the array
-     * Now only those elements within [i-(k-1),i] are in the deque. We then discard
-     * elements smaller than a[i] from the tail. This is because if a[x] <a[i] and x<i,
-     * then a[x] has no chance to be the "max" in [i-(k-1),i], or any other
-     * subsequent window: a[i] would always be a better candidate.
-     *
-     * As a result elements in the deque are ordered in both sequence in array and their value.
-     * At each step the head of the deque is the max element in [i-(k-1),i]
-     * 
      * @param a
      * @param k
      * @return
@@ -35,26 +20,37 @@ public class SlidingWindowMaximum {
         if (a == null || k <= 0) {
             return new int[0];
         }
-        int n = a.length;
-        int[] r = new int[n-k+1];
-        int ri = 0;
-        // store index
-        Deque<Integer> q = new ArrayDeque<>();
-        for (int i = 0; i < a.length; i++) {
-            // remove numbers out of range k
-            while (!q.isEmpty() && q.peek() < i - k + 1) {
-                q.poll();
+        int n    = a.length;
+        int finalArraySize = n-k+1;
+        int[] result = new int[finalArraySize];
+        int resultIndex = 0;
+
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for(int i = 0 ; i<n; i++) {
+            if(!dq.isEmpty() && dq.peek() <= i -k) {
+                dq.poll();
             }
-            // remove smaller numbers in k range as they are useless
-            while (!q.isEmpty() && a[q.peekLast()] < a[i]) {
-                q.pollLast();
+
+            while(!dq.isEmpty() && a[dq.peekLast()] < a[i]) {
+                dq.pollLast();
             }
-            // q contains index... r contains content
-            q.offer(i);
-            if (i >= k - 1) {
-                r[ri++] = a[q.peek()];
+
+            dq.offer(i);
+
+            if(i >= k-1) {
+                result[resultIndex++] = a[dq.peek()];
             }
         }
-        return r;
+
+        return result;
+    }
+
+    public static void main(String [] args) {
+        SlidingWindowMaximum slidingWindowMaximum = new SlidingWindowMaximum();
+        int[] arr = {1,3,-1,-3,5,3,6,7};
+        for(int i : slidingWindowMaximum.maxSlidingWindow(arr, 3)) {
+            System.out.println(i);
+        }
     }
 }
