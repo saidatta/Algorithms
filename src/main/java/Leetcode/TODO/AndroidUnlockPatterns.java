2253239d085354db9e949e2c927b0fad91f94950
vmunnangi <venkata.munnangi@ascentis.com>
2017-07-21 17:09:38 -0700

    Adding solutions for 2 more problems.

--- src/main/java/Leetcode/TODO/AndroidUnlockPatterns.java
+++ src/main/java/Leetcode/TODO/AndroidUnlockPatterns.java
@@ -8,42 +8,42 @@ package Leetcode.TODO;
 public class AndroidUnlockPatterns {
     // cur: the current position
     // remain: the steps remaining
-    public int dfs(boolean[] vis, int[][] skip, int cur, int remain) {
+    public int dfs(boolean[] visited, int[][] skipOverNumber, int cur, int remain) {
         if (remain < 0) {
             return 0;
         }
         if (remain == 0) {
             return 1;
         }
-        vis[cur] = true;
-        int rst = 0;
+        visited[cur] = true;
+        int result = 0;
         for (int i = 1; i <= 9; ++i) {
-            // If vis[i] is not visited and (two numbers are adjacent or skip number is already visited)
-            if (!vis[i] && (skip[cur][i] == 0 || (vis[skip[cur][i]]))) {
-                rst += dfs(vis, skip, i, remain - 1);
+            // If visited[i] is not visited and (two numbers are adjacent or skipOverNumber number is already visited)
+            if (!visited[i] && (skipOverNumber[cur][i] == 0 || (visited[skipOverNumber[cur][i]]))) {
+                result += dfs(visited, skipOverNumber, i, remain - 1);
             }
         }
-        vis[cur] = false;
-        return rst;
+        visited[cur] = false;
+        return result;
     }
 
     public int numberOfPatterns(int m, int n) {
-        // Skip array represents number to skip between two pairs
-        int[][] skip = new int[10][10];
-        skip[1][3] = skip[3][1] = 2;
-        skip[1][7] = skip[7][1] = 4;
-        skip[3][9] = skip[9][3] = 6;
-        skip[7][9] = skip[9][7] = 8;
-        skip[1][9] = skip[9][1] = skip[2][8] = skip[8][2] = skip[3][7] = skip[7][3] = skip[4][6] = skip[6][4] = 5;
+        // Skip array represents number to skipOverNumber between two pairs
+        int[][] skipOverNumber = new int[10][10];
+        skipOverNumber[1][3] = skipOverNumber[3][1] = 2;
+        skipOverNumber[1][7] = skipOverNumber[7][1] = 4;
+        skipOverNumber[3][9] = skipOverNumber[9][3] = 6;
+        skipOverNumber[7][9] = skipOverNumber[9][7] = 8;
+        skipOverNumber[1][9] = skipOverNumber[9][1] = skipOverNumber[2][8] = skipOverNumber[8][2] = skipOverNumber[3][7] = skipOverNumber[7][3] = skipOverNumber[4][6] = skipOverNumber[6][4] = 5;
 
-        boolean[] vis = new boolean[10];
-        int rst = 0;
+        boolean[] visited = new boolean[10];
+        int result = 0;
         // dfs search each length from m to n
         for (int i = m; i <= n; ++i) {
-            rst += dfs(vis, skip, 1, i - 1) * 4;    // 1, 3, 7, 9 are symmetric
-            rst += dfs(vis, skip, 2, i - 1) * 4;    // 2, 4, 6, 8 are symmetric
-            rst += dfs(vis, skip, 5, i - 1);        // 5
+            result += dfs(visited, skipOverNumber, 1, i - 1) * 4;    // 1, 3, 7, 9 are symmetric
+            result += dfs(visited, skipOverNumber, 2, i - 1) * 4;    // 2, 4, 6, 8 are symmetric
+            result += dfs(visited, skipOverNumber, 5, i - 1);        // 5
         }
-        return rst;
+        return result;
     }
 }
