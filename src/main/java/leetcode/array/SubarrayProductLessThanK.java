9f39f95e6bda4aeb9ac5cafd2e8db47072229a55
Venkat Munnangi <venkatmunn@gmail.com>
2023-09-28 20:26:22 -0700

    Added new solutions

--- src/main/java/leetcode/array/SubarrayProductLessThanK.java
+++ src/main/java/leetcode/array/SubarrayProductLessThanK.java
@@ -1,30 +1,55 @@
 package leetcode.array;
 
 /**
- * Created by venkatamunnangi on 9/12/19.
+ * A solution for the problem: https://leetcode.com/problems/subarray-product-less-than-k/
+ * The class provides a method to calculate the number of contiguous subarrays where the product of all the elements
+ * in the subarray is less than k.
  */
 public class SubarrayProductLessThanK {
+
+    /**
+     * Returns the count of subarrays that have a product less than k.
+     *
+     * @param nums Input array of integers.
+     * @param k The threshold for product comparison.
+     * @return The count of subarrays with a product less than k.
+     */
     public int numSubarrayProductLessThanK(int[] nums, int k) {
         if (k == 0) {
             return 0;
         }
 
-        int totalCount = 0;
-        int currentProduct = 1;
-        int left = 0;
-        for (int right = 0; right < nums.length; right++) {
-            currentProduct *= nums[right];
-            while (left <= right && currentProduct >= k) {
-                currentProduct /= nums[left++];
+        // To keep track of the number of valid subarrays.
+        int subarrayCount = 0;
+        // To keep track of the product of the current subarray.
+        int productOfCurrentSubarray = 1;
+        // Left pointer for the sliding window.
+        int leftPointer = 0;
+
+        for (int rightPointer = 0; rightPointer < nums.length; rightPointer++) {
+            productOfCurrentSubarray *= nums[rightPointer];
+
+            // Adjust the left pointer to keep the product under k.
+            while (leftPointer <= rightPointer && productOfCurrentSubarray >= k) {
+                productOfCurrentSubarray /= nums[leftPointer++];
             }
-            totalCount += right - left + 1;
+
+            // Add the number of subarrays ending at the current position.
+            // this works because you're counting the index everytime it occurs in the sliding window
+            // in below example, you first count 10, then you count 10 again when it is with 10, 5
+            subarrayCount += rightPointer - leftPointer + 1;
         }
-        return totalCount;
+
+        return subarrayCount;
     }
 
-    public static void main(String [] args) {
-        SubarrayProductLessThanK subarrayProductLessThanK = new SubarrayProductLessThanK();
+    public static void main(String[] args) {
+        SubarrayProductLessThanK solution = new SubarrayProductLessThanK();
+        int[] sampleInput = {10, 5, 2, 6};
+        int threshold = 100;
 
-        System.out.println(subarrayProductLessThanK.numSubarrayProductLessThanK(new int[]{10, 5, 2, 6},100));
+        // Print the result for the sample input.
+        System.out.println(solution.numSubarrayProductLessThanK(sampleInput, threshold));
     }
 }
+
